47e99260cdf0d442cee3a2e403ab45c9b3161a0c
Vijay-devx <yadavvijay3107@gmail.com>
2026-02-09 21:22:59 +0530

    TwoSumSorted.java

---
+++ two_pointers/TwoSumSorted.java
@@ -0,0 +1,74 @@
+
+/* Problem: 
+   Given a sorted array of integers and a target sum, check whether there exists a pair of elements whose sum equals the target.
+   
+   input: n, sorted array, target 
+   output: Found / Not Found
+*/
+
+import java.util.Scanner;
+
+public class TwoSumSorted 
+{
+    static boolean twoSum(int[] arr, int target, int n)
+    {
+        int left = 0;
+        int right = n-1;
+
+        while(left<right)
+        {
+            int sum = arr[left]+arr[right];
+            
+            if(sum==target)
+            {
+                return true;
+            }
+            else if(sum<target)                  // if after combining smallest number that is arr[left] with largest number that is arr[right], sum<target, then any other pair involving arr[left] will be lesser than target, hence we increase left, that's left++;
+            {
+                left++;
+                continue;
+            }
+
+            else if(sum>target)                  // if after combining largest number that is arr[right] with smallest number thats arr[left], sum>target, then any other pair involving arr[right] will be larger than target, hence we reduce the arr[right] i.e. right--;
+            {
+                right--;
+                continue;
+            }
+        }
+
+        return false;                           // return false if the loop exits without finding target sum.
+
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the no of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Please enter elements of sorted array: ");
+        for(int i=0; i<n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Please enter the target sum: ");
+        int target = sc.nextInt();
+
+        boolean result = twoSum(arr, target, n);
+
+        if(result == true)
+        {
+            System.out.println("Found");
+        }
+        else
+        {
+            System.out.println("Not Found");
+        }
+
+        sc.close();
+
+    }
+    
+}
